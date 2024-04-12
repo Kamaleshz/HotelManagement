84d0d275a63cf5002c9f59bade43189e85a25e50
Muhammed Ali Masood <muhammedali.masood@kanini.com>
2024-04-12 15:16:37 +0530

    FrontEnd Setup and Folder Structure done

---
+++ Frontend/hotelmanagementfrontend/src/Shared/PrimaryNavbar.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Box from "@mui/material"
+const PrimaryNavbar = (props) => {
+  return (
+    <div>
+       <Box>
+            <Box>
+                Taj Hotel
+            </Box>
+            <Box>
+                <Box>
+                    <a>Admin</a>
+                    <a>Rooms</a>
+                    <a>My Bookings</a>
+                    <a>Contact Us</a>
+                    <a>Login</a>
+
+                </Box>
+                <Box>
+                    Login
+                </Box>
+            </Box>
+       </Box>
+    </div>
+  )
+};
+
+export default PrimaryNavbar;
